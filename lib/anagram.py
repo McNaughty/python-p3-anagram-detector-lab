f758a29624c6199bb722eb280c59869ff4f1383f
Winnie <wimacoj@gmail.com>
2024-01-26 10:02:25 +0300

    Completed assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, words):
+        return [w for w in words if sorted(w.lower()) == sorted(self.word) and w.lower() != self.word]
+
+# Example usage:
+if __name__ == "__main__":
+    word_to_check = "listen"
+    possible_anagrams = ['enlists', 'google', 'inlets', 'banana']
+
+    anagram_instance = Anagram(word_to_check)
+    result = anagram_instance.match(possible_anagrams)
+
+    print(result)
